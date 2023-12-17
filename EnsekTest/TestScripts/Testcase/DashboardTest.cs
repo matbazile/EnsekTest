bc8acedfc8a1c6d8c34f4b8c656b529e326a6a46
Mathieu BAZILE <mathieubazile@gmail.com>
2023-12-17 13:23:10 +0000

    Added scenario description

--- EnsekTest/TestScripts/Testcase/DashboardTest.cs
+++ EnsekTest/TestScripts/Testcase/DashboardTest.cs
@@ -14,13 +14,27 @@ namespace EnsekTest.TestScripts.Testcase
     public class DashboardTest
     {
         [TestMethod]
-        public void DasbordSignIn()
+        public void Register()
         {
+
+            //Scenario:
+            //The user land on the website and try to register using a wrong credential
+
+            //Given: I am on the company website
             NavigationHelper.NavigateToUrl(ObjectRepository.Config.GetWebsite());
             HomePage homepage = new HomePage();
 
-           
-            Assert.IsTrue(HomePage.Submitted, "Not Submitter");
+            Assert.IsTrue(HomePage.homepagetitle, "User is not on ENSEK website");
+
+            //When I register using wrong credential
+            homepage.GoToRegister();
+
+            RegisterPage register = new RegisterPage();
+            register.GetregisterUsingWrongCredential();
+
+            //Then: User is not register
+            Assert.IsTrue(RegisterPage.wrongcredential, "Wrong credential used");
+
         }
     }
 }
